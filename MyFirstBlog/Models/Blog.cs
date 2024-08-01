d71aacd9effa0abfbdf2d75ff03664fde2f46d00
FoOtBaLL3R <pavlo.23.94@gmail.com>
2024-07-31 19:36:36 -0600

    Created project and added models

---
+++ MyFirstBlog/Models/Blog.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstBlog.Models
+{
+    public class Blog(DbContextOptions<Blog> options) : IdentityDbContext<User>(options)
+    {
+    }
+}
