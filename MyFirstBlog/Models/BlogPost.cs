a2ec99770c3e38b530fd10a145303e75781d9e2f
FoOtBaLL3R <pavlo.23.94@gmail.com>
2024-08-01 18:15:31 -0600

    add docker container Postgres, new models and configured program file v2

---
+++ MyFirstBlog/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstBlog.Models
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public User User { get; set; }
+        public Guid IdUser { get; set; }
+    }
+}
